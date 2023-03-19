5dfd2a27583e2645cf101b981b3db6d88b75f36c
55selcukozdemir <55selcukozdemir@gmail.com>
2023-03-19 22:26:46 +0300

    genel değişimler pek de bir önemi yok

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-
+import { useState } from "react";
+import {SidebarProvider } from "./context/Context";
+import Content from "./general/Content";
+import Sidebar from "./general/Sidebar";
+import Appbar from "./general/Appbar"
 
 const App = () => {
-    return(
-        <h1>merhaba dünya</h1>
-    )
-}
+  return (
+    <>
+      <SidebarProvider>
+        <Appbar/>
+        <Sidebar />
+        <Content>
+          <div>Content</div>
+        </Content>
+      </SidebarProvider>
+    </>
+  );
+};
 
-export default App
+export default App;
